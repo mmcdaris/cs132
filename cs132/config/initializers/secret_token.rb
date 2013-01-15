eb4b32dce7221c750108484315b1da6d7660cc56
Morgan McDaris <morganmcdaris@yahoo.com>
2013-01-14 20:06:02 -0800

    booting up!

---
+++ cs132/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs132::Application.config.secret_token = '7c4be50a9b2898f2f3d1733218004dec519ff62517780aa5e7de47b949657077d23bc72218af91156ab91ea4ec3f4d9d1de7d4c7946ca786943898fa4d86a2f2'
